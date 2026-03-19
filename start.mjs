134b0f669e226221587046c806bfacd2885ab8eb
gmalickovski <gui.malickovski@gmail.com>
2026-03-19 15:27:51 -0300

    fix: adiciona leitura do .env no PM2 e atualiza deploy

--- start.mjs
+++ start.mjs
@@ -1,3 +1,7 @@
 import dotenv from 'dotenv';
+
+// 1. Roda a função e carrega as 22 variáveis na memória PRIMEIRO
 dotenv.config({ path: '/var/www/webapp/nome-magnetico/.env' });
-import './dist/server/entry.mjs';
+
+// 2. SÓ DEPOIS que o ambiente está pronto, "chama" o Astro dinamicamente
+import('./dist/server/entry.mjs');
