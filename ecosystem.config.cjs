8a4d5ec501cab970849801ddaa659e6de0d61a7d
gmalickovski <gui.malickovski@gmail.com>
2026-03-19 13:29:40 -0300

    fix: adiciona leitura do .env no PM2 e atualiza deploy

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -5,8 +5,10 @@ module.exports = {
       script: './dist/server/entry.mjs',
       instances: 'max',
       exec_mode: 'cluster',
+      node_args: '--env-file=.env', // <-- ESSA É A LINHA MÁGICA QUE FALTAVA
       env: {
         PORT: 4321,
+        HOST: '0.0.0.0',
         NODE_ENV: 'production',
       },
     },
